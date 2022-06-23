0345698fa2174ffbf1cf8f7c14b0a6d397f8dfb3
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2022-06-23 15:33:55 +0300

    exercise

--- python_advanced/task_exam/aladdin_s_gifts.py
+++ python_advanced/task_exam/aladdin_s_gifts.py
@@ -1,12 +1,12 @@
 from collections import deque
-# 58/100
+#83/100
 materials = [int(x) for x in input().split(' ')]
 magics = deque([int(x) for x in input().split()])
 gemstone = False
 porcelain_sculpture = False
 gold = False
 diamond = False
-result = {'Gemstone': 0, 'Porcelain Sculpture': 0, 'Gold': 0, 'Diamond Jewellery': 0}
+result = {'Diamond Jewellery': 0, 'Gemstone': 0, 'Gold': 0, 'Porcelain Sculpture': 0}
 while materials and magics:
     material = materials.pop()
     magic = magics.popleft()
@@ -15,9 +15,9 @@ while materials and magics:
         if sum_number % 2 == 0:
             sum_number = (material * 2) + (magic * 3)
         else:
-            sum_number = (material * 2) + (magic * 2)
+            sum_number = (material + magic) * 2
     if sum_number > 499:
-        sum_number = (material / 2) + (magic / 2)
+        sum_number = (material + magic) / 2
 
     if 100 <= sum_number <= 199:
         result['Gemstone'] += 1
@@ -28,7 +28,7 @@ while materials and magics:
     if 300 <= sum_number <= 399:
         result['Gold'] += 1
         gold = True
-    if 400 <= sum_number < 499:
+    if 400 <= sum_number <= 499:
         result['Diamond Jewellery'] += 1
         diamond = True
 
@@ -36,6 +36,7 @@ if gemstone and porcelain_sculpture or gold and diamond:
     print('The wedding presents are made!')
 else:
     print('Aladdin does not have enough wedding presents.')
+
 if materials:
     mat = map(str, materials)
     print(f"Materials left: {''.join(mat)}")
@@ -48,3 +49,4 @@ if magics:
 for key, value in result.items():
     if value != 0:
         print(f"{key}: {value}")
+
